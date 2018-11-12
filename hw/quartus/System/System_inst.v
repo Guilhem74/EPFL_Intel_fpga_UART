265f7d60ba68647575ab81b12a5f0cb60e81ee63
Guilhem AZZANO <g.azzano74@gmail.com>
2018-11-12 23:42:43 +0100

    clean files

--- hw/quartus/System/System_inst.v
+++
@@ -1,8 +0,0 @@
-	System u0 (
-		.clk_clk                      (<connected-to-clk_clk>),                      //                   clk.clk
-		.fpga_uart_custom_0_rx_rx_in  (<connected-to-fpga_uart_custom_0_rx_rx_in>),  // fpga_uart_custom_0_rx.rx_in
-		.fpga_uart_custom_0_tx_tx_out (<connected-to-fpga_uart_custom_0_tx_tx_out>), // fpga_uart_custom_0_tx.tx_out
-		.reset_reset_n                (<connected-to-reset_reset_n>),                //                 reset.reset_n
-		.fpga_uart_debug_readdata     (<connected-to-fpga_uart_debug_readdata>)      //       fpga_uart_debug.readdata
-	);
-
